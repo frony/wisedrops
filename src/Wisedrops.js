9748b13256b6f618fdddbf4812f01277d7db9217
Marcos Frony <mfrony@yahoo.com>
2022-05-17 20:07:10 -0700

    Animate opacity

--- src/Wisedrops.js
+++ src/Wisedrops.js
@@ -1,13 +1,22 @@
 import { useState, useEffect } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 const quotes = require('./data/quotes.json');
+
+const ANIMATION_INTERVAL = 10;
+
+const flickerAnimation = keyframes`
+  0% { opacity: 0; }
+  50%  { opacity:1; }
+  100% { opacity: 0; }
+`;
 
 const Container = styled.div`
 	display: block;
   width: 300px;
   border: #000000 1px solid;
   margin: 20px;
+  min-height: 250px;
 `;
 
 const Title = styled.div`
@@ -21,16 +30,29 @@ const Title = styled.div`
 
 const Wisdom = styled.div`
   font-size: 16px;
-  margin: 20px 10px
+  margin: 20px 10px;
+  opacity:1;
+  animation: ${flickerAnimation} ${ANIMATION_INTERVAL}s infinite;
 `;
 
 const Author = styled.div`
   font-size: 12px;
   font-style: italic;
   margin: 0 10px 20px;
-  //padding-top: 10px;
   text-align: left;
-  //border-top: 1px solid #282c34;
+  opacity:1;
+  animation: ${flickerAnimation} ${ANIMATION_INTERVAL}s infinite;
+`;
+
+// const Panel = styled.div`
+//   display: inline-block;
+//   vertical-align: middle;
+//   border: red 1px solid;
+// `;
+
+const AnimateFlicker = styled.div`
+  opacity:1;
+  animation: ${flickerAnimation} ${ANIMATION_INTERVAL}s infinite;
 `;
 
 export const Wisedrops = () => {
@@ -52,13 +74,15 @@ export const Wisedrops = () => {
     }
     setIndex(newIndex);
     setQuote(quotes[newIndex]);
-  }, 5000);
+  }, ANIMATION_INTERVAL * 1000);
   return (
       <Container>
         <Title>Wisedrops</Title>
-        {/*<p>{index}</p>*/}
-        <Wisdom>{ quote.wisdom }</Wisdom>
-        <Author>{ quote.author }</Author>
+        {/*<AnimateFlicker>{index}</AnimateFlicker>*/}
+        {/*<Panel>*/}
+          <Wisdom>{ quote.wisdom }</Wisdom>
+          <Author>{ quote.author }</Author>
+        {/*</Panel>*/}
       </Container>
   );
 };
